07b5d4f2527fbfec8826ee81ae0549c1c0cfea32
Felipe Capellari <42899982+capellari@users.noreply.github.com>
2025-07-23 15:44:32 +0000

    Refatora a declaração de variáveis no cadastro das cartas para simplificar a inicialização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,15 +8,9 @@
 // Nível novato
 
 int main() {
-    int populacao_carta_1 = 0;
-    int pontos_turisticos_carta_1 = 0;
-    float area_carta_1 = 0.0;
-    float pib_carta_1 = 0.0;
+    int populacao_carta_1, pontos_turisticos_carta_1, populacao_carta_2, pontos_turisticos_carta_2 = 0;
+    float area_carta_1, pib_carta_1, area_carta_2, pib_carta_2 = 0.0;
 
-    int populacao_carta_2 = 0;
-    int pontos_turisticos_carta_2 = 0;
-    float area_carta_2 = 0.0;
-    float pib_carta_2 = 0.0;
 
 
     printf("Bem-vindo ao desafio Super Trunfo - Países! No jogo Super Trunfo, os jogadores comparam os atributos das cartas para determinar a mais forte. \nO tema deste Super Trunfo é Países, onde você comparará os atributos das cidades.");
@@ -46,6 +40,9 @@ int main() {
     printf("Pontos Turísticos: ");
     scanf("%d", &pontos_turisticos_carta_2);
 
+    // Exibindo os dados das cartas cadastradas
+    printf("\nCartas cadastradas com sucesso!\n");
+    printf("Dados das cartas:\n");
 
     printf("\nPrimeira carta: ");
     printf("\nPopulação: %d", populacao_carta_1);
